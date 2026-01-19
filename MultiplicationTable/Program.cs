ec7e1ece49c19d827f4ec539d1844861619d1277
nevinjreji <nevinjreji@gmail.com>
2026-01-19 10:15:11 +0530

    MultiplicationTable

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static int[] GetTableRow(int n, int upto)
+    {
+        int[] row = new int[upto];
+
+        for (int i = 1; i <= upto; i++)
+        {
+            row[i - 1] = n * i;
+        }
+
+        return row;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter the number (n): ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the limit (upto): ");
+        int upto = Convert.ToInt32(Console.ReadLine());
+
+        int[] result = GetTableRow(n, upto);
+
+        Console.WriteLine("\nMultiplication table row:");
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            Console.Write(result[i]);
+
+            if (i < result.Length - 1)
+                Console.Write(", ");
+        }
+    }
+}
