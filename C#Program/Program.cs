c9b74a3e8eaf868c2e81da7e61a4bb0ed7bd3fe8
nevinjreji <nevinjreji@gmail.com>
2026-01-19 09:53:17 +0530

    Initial commit - added C# projects

---
+++ C#Program/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static bool IsVowel(char ch)
+    {
+        ch = char.ToLower(ch);
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+
+    static void Main()
+    {
+        string first = Console.ReadLine()!;
+        string second = Console.ReadLine()!;
+
+        string temp = "";
+
+        // remove common consonants
+        foreach (char ch in first)
+        {
+            char c = char.ToLower(ch);
+
+            if (!IsVowel(c) && second.ToLower().Contains(c))
+                continue;
+
+            temp += ch;
+        }
+
+        string result = "";
+
+        // remove consecutive duplicates
+        for (int i = 0; i < temp.Length; i++)
+        {
+            if (i == 0 || temp[i] != temp[i - 1])
+                result += temp[i];
+        }
+
+        Console.WriteLine(result);
+    }
+}
