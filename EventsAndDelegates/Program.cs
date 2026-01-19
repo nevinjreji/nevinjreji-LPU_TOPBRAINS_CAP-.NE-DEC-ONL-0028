a5c7a8a01950cc58f17a4e8d041fb128fe7d7cd3
nevinjreji <nevinjreji@gmail.com>
2026-01-19 12:30:38 +0530

    Delegates2

---
+++ EventsAndDelegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    public void fun()
+    {
+        System.Console.WriteLine("Function Body");
+    }
+
+    //UNICAST DELEGATES
+    public delegate void mydelegate();
+
+    public static void Main()
+    {
+        Program pObj = new Program();
+        mydelegate mydelObj = new mydelegate(pObj.fun);
+
+        mydelObj();
+    }
+}
