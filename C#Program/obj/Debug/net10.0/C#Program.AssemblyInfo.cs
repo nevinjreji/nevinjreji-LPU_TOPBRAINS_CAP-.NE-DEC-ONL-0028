6b42f078e0465e012cdb83a62aabbee7c7045241
nevinjreji <nevinjreji@gmail.com>
2026-01-30 10:07:10 +0530

    SumOfPositiveIntegers

--- C#Program/obj/Debug/net10.0/C#Program.AssemblyInfo.cs
+++ C#Program/obj/Debug/net10.0/C#Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec7e1ece49c19d827f4ec539d1844861619d1277")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41249e33faf4798cea388253c8c32eddfe802b21")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ SumofPositiveIntegers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var nums = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        long sum = 0;
+
+        foreach (var x in nums)
+        {
+            if (x == 0) break;
+            if (x < 0) continue;
+            sum += x;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
