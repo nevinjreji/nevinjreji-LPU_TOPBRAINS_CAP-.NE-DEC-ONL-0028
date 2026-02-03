99550869017baf9abe85575c06218fcf77cda978
nevinjreji <nevinjreji@gmail.com>
2026-02-03 14:23:23 +0530

    Conversion

---
+++ Conversion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    public static double ConvertFeetToCentimeters(int feet)
+    {
+        double centimeters = feet * 30.48;
+        return Math.Round(centimeters, 2, MidpointRounding.AwayFromZero);
+    }
+
+    static void Main(string[] args)
+    {
+        int feet = int.Parse(Console.ReadLine());
+        double result = ConvertFeetToCentimeters(feet);
+        Console.WriteLine(result);
+    }
+}
